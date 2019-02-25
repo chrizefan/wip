6e33c12c6bb94fd1ffac52fadc8847fed43c439c
Chris Stefan <chris.stefan@gmail.com>
2019-02-25 10:54:33 -0500

    getLocationPermission
    permission request

--- app/src/main/java/com/codineasy/wip/MapsActivity.java
+++ app/src/main/java/com/codineasy/wip/MapsActivity.java
@@ -1,9 +1,15 @@
 package com.codineasy.wip;
 
+import android.Manifest;
 import android.app.Dialog;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
@@ -19,19 +25,33 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
+    private static final String TAG = "MapsActivity";
+
     static final int GOOGLE_PLAY_SERVICE_UPDATED_ERROR_REQUEST = 0;
+    static final int LOCATION_PERMISSION_REQUEST = 1;
+
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private Boolean mLocationPermissionGranted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        if(isGoogleServicesUpdated()) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_maps);
+
+            getLocationPermission();
+        }
+    }
+
+    private void initMap(){
+        Log.d(TAG, "initMap: initializing map");
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
-
-
 
     public boolean isGoogleServicesUpdated(){
         Log.d("Update", "isGoogleServicesUpdated: checking Google Play Services");
@@ -40,18 +60,67 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         if(available == ConnectionResult.SUCCESS){
             //Google Play Services is up to date
-            Log.d("Update", "isGoogleServicesUpdated: Google Play Services ");
+            Log.d("Update", "isGoogleServicesUpdated: Google Play Services updated");
             return true;
         }
-        else{
+        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
             //An error occurred or Google Play Services is not up to date
-            Log.d("Update", "isGoogleServicesUpdated: an error occurred");
-            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MapsActivity.this, available, GOOGLE_PLAY_SERVICE_UPDATED_ERROR_REQUEST);
+            Log.d("Update", "isGoogleServicesUpdated: error");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(this, available,
+                    GOOGLE_PLAY_SERVICE_UPDATED_ERROR_REQUEST);
             dialog.show();
             return false;
+        } else{
+            Toast.makeText(this, "Unable to make map requests", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+    private void getLocationPermission(){
+        Log.d(TAG, "getLocationPermission: getting location permissions");
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION};
+
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                    COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                mLocationPermissionGranted = true;
+            } else{
+                ActivityCompat.requestPermissions(this,
+                        permissions,
+                        LOCATION_PERMISSION_REQUEST);
+            }
+        }else{
+            ActivityCompat.requestPermissions(this,
+                    permissions,
+                    LOCATION_PERMISSION_REQUEST);
         }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d(TAG, "onRequestPermissionsResult: called");
+        mLocationPermissionGranted = false;
+
+        switch(requestCode){
+            case LOCATION_PERMISSION_REQUEST:{
+                if(grantResults.length > 0){
+                    for(int grantResult : grantResults) {
+                        if (grantResult != PackageManager.PERMISSION_GRANTED) {
+                            Log.d(TAG, "onRequestPermissionsResult: permission failed");
+                            mLocationPermissionGranted = false;
+                            return;
+                        }
+                    }
+                    Log.d(TAG, "onRequestPermissionsResult: permission granted");
+                    mLocationPermissionGranted = true;
+                    //initialize map
+                    initMap();
+                }
+            }
+        }
+    }
 
     /**
      * Manipulates the map once available.
@@ -64,9 +133,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        Toast.makeText(this, "Map is Ready", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onMapReady: map ready");
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
+        // Add a marker in Montreal and move the camera
         LatLng montreal = new LatLng(45, 73);
         mMap.addMarker(new MarkerOptions().position(montreal).title("Marker in Montreal"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(montreal));
