81dc458e803bbd21579476ddd2b2efcd635b2a37
konidia <nafiz.islam1000@gmail.com>
2019-03-09 20:37:39 -0500

    Replace "WeatherLogs" with "Details"
    -for consistency and ease of UI development

--- app/src/main/java/com/codineasy/wip/Details.java
+++ app/src/main/java/com/codineasy/wip/Details.java
@@ -3,11 +3,11 @@ package com.codineasy.wip;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class WeatherLogs extends AppCompatActivity {
+public class Details extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_weather_logs);
+        setContentView(R.layout.activity_details);
     }
 }
